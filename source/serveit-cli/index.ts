2722c14c7127da1ce0d623a4ec09042b1e765a9e
Joel Ek <contact@joelek.se>
2020-02-28 16:22:36 +0100

    Place CLI in run function.

--- source/serveit-cli/index.ts
+++ source/serveit-cli/index.ts
@@ -1,30 +1,31 @@
 import { serve } from "../serveit-lib";
 
-let options = {
-	root: "./",
-	port: 8000
-};
-
-let found_unrecognized_argument = false;
-
-for (let arg of process.argv.slice(2)) {
-	let parts: RegExpExecArray | null = null;
-	if (false) {
-	} else if ((parts = /^--root=(.+)$/.exec(arg)) !== null) {
-		options.root = parts[1];
-	} else if ((parts = /^--port=([0-9]+)$/.exec(arg)) !== null) {
-		options.port = Number.parseInt(parts[1]);
+function run(): void {
+	let options = {
+		root: "./",
+		port: 8000
+	};
+	let found_unrecognized_argument = false;
+	for (let arg of process.argv.slice(2)) {
+		let parts: RegExpExecArray | null = null;
+		if (false) {
+		} else if ((parts = /^--root=(.+)$/.exec(arg)) !== null) {
+			options.root = parts[1];
+		} else if ((parts = /^--port=([0-9]+)$/.exec(arg)) !== null) {
+			options.port = Number.parseInt(parts[1]);
+		} else {
+			found_unrecognized_argument = true;
+			process.stderr.write("Argument \"" + arg + "\" was not recognized!\n");
+		}
+	}
+	if (found_unrecognized_argument) {
+		process.stderr.write("Arguments:\n");
+		process.stderr.write("	--root=string\n");
+		process.stderr.write("	--port=number\n");
+		process.exit(0);
 	} else {
-		found_unrecognized_argument = true;
-		process.stderr.write("Argument \"" + arg + "\" was not recognized!\n");
+		serve(options.root, options.port);
 	}
 }
 
-if (found_unrecognized_argument) {
-	process.stderr.write("Arguments:\n");
-	process.stderr.write("	--root=string\n");
-	process.stderr.write("	--port=number\n");
-	process.exit(0);
-} else {
-	serve(options.root, options.port);
-}
+run();
