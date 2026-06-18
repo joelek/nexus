5407f16e368b23618f6bd9cb36700e5b8d1320ac
Joel Ek <contact@joelek.se>
2026-06-18 13:46:58 +0200

    Add more tests for proxy module.

--- source/lib/proxy.test.ts
+++ source/lib/proxy.test.ts
@@ -5,7 +5,9 @@ import * as proxy from "./proxy";
 wtf.test(`Server should immediately reset connections sending bad proxy headers.`, async (assert) => {
 	return new Promise<void>((resolve, reject) => {
 		setTimeout(reject, 1 * 1000);
-		let server = proxy.createServer({}, (socket, header) => {});
+		let server = proxy.createServer({
+			trustedRemoteAddresses: []
+		}, (socket, header) => {});
 		server.listen(undefined, () => {
 			let address = proxy.getServerAddress(server);
 			let socket = libnet.connect({
@@ -20,7 +22,63 @@ wtf.test(`Server should immediately reset connections sending bad proxy headers.
 				}
 			});
 			socket.once("connect", () => {
-				socket.write("PROXY 1337");
+				socket.write("PROXY\r\n");
+			});
+		});
+	});
+});
+
+wtf.test(`Server should pass PROXY header when remote address is trusted.`, async (assert) => {
+	return new Promise<void>((resolve, reject) => {
+		setTimeout(reject, 1 * 1000);
+		let server = proxy.createServer({
+			trustedRemoteAddresses: ["localhost"]
+		}, (socket, header) => {
+			if (header != null) {
+				resolve();
+			} else {
+				reject();
+			}
+		});
+		server.listen(undefined, () => {
+			let address = proxy.getServerAddress(server);
+			let socket = libnet.connect({
+				port: address.port
+			});
+			socket.on("error", (error) => {}); // NOTE: Prevent errors from being thrown.
+			socket.once("close", (had_error) => {
+				reject();
+			});
+			socket.once("connect", () => {
+				socket.write("PROXY TCP4 0.1.2.3 4.5.6.7 8 9\r\n");
+			});
+		});
+	});
+});
+
+wtf.test(`Server should not pass PROXY header when remote address is untrusted.`, async (assert) => {
+	return new Promise<void>((resolve, reject) => {
+		setTimeout(reject, 1 * 1000);
+		let server = proxy.createServer({
+			trustedRemoteAddresses: []
+		}, (socket, header) => {
+			if (header != null) {
+				reject();
+			} else {
+				resolve();
+			}
+		});
+		server.listen(undefined, () => {
+			let address = proxy.getServerAddress(server);
+			let socket = libnet.connect({
+				port: address.port
+			});
+			socket.on("error", (error) => {}); // NOTE: Prevent errors from being thrown.
+			socket.once("close", (had_error) => {
+				reject();
+			});
+			socket.once("connect", () => {
+				socket.write("PROXY TCP4 0.1.2.3 4.5.6.7 8 9\r\n");
 			});
 		});
 	});
