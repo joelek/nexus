82ac3dddcaee1c90bbb20b7d1c1c90eb239b34af
Joel Ek <contact@joelek.se>
2026-06-20 13:22:20 +0200

    Add HTTPS request proxying test for HTTP server.

--- source/lib/index.test.ts
+++ source/lib/index.test.ts
@@ -4,7 +4,7 @@ import * as wtf from "@joelek/wtf";
 import * as index from "./index";
 import * as utils from "./utils";
 
-wtf.test(`Server should support http protocol.`, async (assert) => {
+wtf.test(`HTTP should support HTTP request proxying.`, async (assert) => {
 	return new Promise<void>((resolve, reject) => {
 		setTimeout(reject, 5 * 1000);
 		let server = libhttp.createServer({});
@@ -26,7 +26,7 @@ wtf.test(`Server should support http protocol.`, async (assert) => {
 			proxy.on("listening", () => {
 				let proxyAddress = utils.getServerAddress(proxy);
 				let request = libhttp.request({
-					port: proxyAddress.port,
+					port: proxyAddress.port
 				});
 				request.on("finish", () => {});
 				request.on("response", (response) => {
@@ -37,10 +37,46 @@ wtf.test(`Server should support http protocol.`, async (assert) => {
 				request.on("close", () => {});
 				request.end();
 			});
-			proxy.listen({
-				port: 8080,
-				host: "0.0.0.0"
+			proxy.listen(undefined, "0.0.0.0");
+		});
+		server.listen(undefined);
+	});
+});
+
+wtf.test(`HTTP server should support HTTPS request proxying.`, async (assert) => {
+	return new Promise<void>((resolve, reject) => {
+		setTimeout(reject, 5 * 1000);
+		let server = libhttps.createServer({});
+		server.on("request", (request, response) => {
+			response.writeHead(404);
+			response.end();
+		});
+		server.on("listening", () => {
+			let serverAddress = utils.getServerAddress(server);
+			let options: index.Options = {
+				domains: [
+					{
+						root: `https://localhost:${serverAddress.port}`
+					}
+				]
+			};
+			let config = index.createConfigFromOptions(options);
+			let proxy = index.createHttpServer(config, options);
+			proxy.on("listening", () => {
+				let proxyAddress = utils.getServerAddress(proxy);
+				let request = libhttp.request({
+					port: proxyAddress.port
+				});
+				request.on("finish", () => {});
+				request.on("response", (response) => {
+					response.on("data", () => {});
+					response.on("end", () => {});
+					response.on("close", resolve);
+				});
+				request.on("close", () => {});
+				request.end();
 			});
+			proxy.listen(undefined, "0.0.0.0");
 		});
 		server.listen(undefined);
 	});
