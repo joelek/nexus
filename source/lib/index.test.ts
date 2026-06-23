756f18bd15b545ba4fb58634f9b80a71c5cb55c9
Joel Ek <contact@joelek.se>
2026-06-23 13:34:11 +0200

    Add test for PROXY connection proxying.

--- source/lib/index.test.ts
+++ source/lib/index.test.ts
@@ -165,3 +165,74 @@ wtf.test(`HTTPS server should support PIPE connection proxying.`, async (assert)
 		}
 	]);
 });
+
+wtf.test(`HTTPS server should support PROXY connection proxying.`, async (assert) => {
+	let events = new Array<wtf.data.SerializableData>();
+	await new Promise<void>((resolve, reject) => {
+		setTimeout(reject, 5 * 1000);
+		let server = libhttp.createServer({});
+		server.on("request", (request, response) => {
+			response.writeHead(404);
+			response.end();
+		});
+		server.on("listening", () => {
+			let serverAddress = utils.getServerAddress(server);
+			let options: index.Options = {
+				domains: [
+					{
+						root: `http://localhost:${serverAddress.port}`
+					}
+				],
+				trust: [
+					"localhost"
+				],
+				sign: true
+			};
+			let config = index.createConfigFromOptions(options);
+			let inner = index.createHttpsServer(config, options);
+			inner.on("listening", () => {
+				let innerAddress = utils.getServerAddress(inner);
+				let options: index.Options = {
+					domains: [
+						{
+							root: `proxy://localhost:${innerAddress.port}`
+						}
+					],
+					trust: [
+						"localhost"
+					]
+				};
+				let config = index.createConfigFromOptions(options);
+				let outer = index.createHttpsServer(config, options);
+				outer.on("listening", () => {
+					let outerAddress = utils.getServerAddress(outer);
+					let request = libhttps.request({
+						port: outerAddress.port,
+						rejectUnauthorized: false
+					});
+					request.on("finish", () => {});
+					request.on("response", (response) => {
+						events.push({
+							type: "request.response",
+							status: response.statusCode
+						});
+						response.on("data", () => {});
+						response.on("end", () => {});
+						response.on("close", resolve);
+					});
+					request.on("close", () => {});
+					request.end();
+				});
+				outer.listen(undefined, "0.0.0.0");
+			});
+			inner.listen(undefined, "0.0.0.0");
+		});
+		server.listen(undefined, "0.0.0.0");
+	});
+	assert.equals(events, [
+		{
+			type: "request.response",
+			status: 404
+		}
+	]);
+});
