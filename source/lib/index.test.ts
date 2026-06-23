3863ea58445e57fffebf9191bd0baa6767961cf6
Joel Ek <contact@joelek.se>
2026-06-23 13:03:23 +0200

    Add test for PIPE connection proxying.

--- source/lib/index.test.ts
+++ source/lib/index.test.ts
@@ -111,3 +111,57 @@ wtf.test(`HTTP server should support HTTPS request proxying.`, async (assert) =>
 		}
 	]);
 });
+
+wtf.test(`HTTPS server should support PIPE connection proxying.`, async (assert) => {
+	let events = new Array<wtf.data.SerializableData>();
+	await new Promise<void>((resolve, reject) => {
+		setTimeout(reject, 5 * 1000);
+		let server = libhttps.createServer(CREDENTIALS);
+		server.on("request", (request, response) => {
+			response.writeHead(404);
+			response.end();
+		});
+		server.on("listening", () => {
+			let serverAddress = utils.getServerAddress(server);
+			let options: index.Options = {
+				domains: [
+					{
+						root: `pipe://localhost:${serverAddress.port}`
+					}
+				],
+				trust: [
+					"localhost"
+				]
+			};
+			let config = index.createConfigFromOptions(options);
+			let proxy = index.createHttpsServer(config, options);
+			proxy.on("listening", () => {
+				let proxyAddress = utils.getServerAddress(proxy);
+				let request = libhttps.request({
+					port: proxyAddress.port,
+					rejectUnauthorized: false
+				});
+				request.on("finish", () => {});
+				request.on("response", (response) => {
+					events.push({
+						type: "request.response",
+						status: response.statusCode
+					});
+					response.on("data", () => {});
+					response.on("end", () => {});
+					response.on("close", resolve);
+				});
+				request.on("close", () => {});
+				request.end();
+			});
+			proxy.listen(undefined, "0.0.0.0");
+		});
+		server.listen(undefined, "0.0.0.0");
+	});
+	assert.equals(events, [
+		{
+			type: "request.response",
+			status: 404
+		}
+	]);
+});
